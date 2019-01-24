631ee9732a4a79b20811865f42b018fd702ab068
kmietotwy <kmietotwy@gmail.com>
2019-01-23 21:13:20 -1000

    changed fullname

--- js/app.js
+++ js/app.js
@@ -13,6 +13,9 @@ spanElem2.innerHTML = 23;
 
 //3. In the div id data section, target the div element with the id of 'fullname' and change Full Name to Ronald McDonald.
 
+var fullNameElem = document.getElementById('fullname');
+fullNameElem.innerHTML = 'Ronald McDonald';
+
 //4. In the div id data section, target the div element with the id of 'age' and change Age to 63 Years Old.
 
 //5. In the div id data section, create a div with an id of job and give it the following information: Clown and Restauranteur.

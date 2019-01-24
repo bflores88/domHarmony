958f0f439cc5553a5a391d3d40266bcfd7b28b83
kmietotwy <kmietotwy@gmail.com>
2019-01-23 21:26:07 -1000

    added hobbies

--- js/app.js
+++ js/app.js
@@ -30,6 +30,12 @@ data.appendChild(newDiv1);
 
 //6. In the div id data section, create a div with an id of hobbies and give it the following information: Long romantic walks on the beach, candle light Big Mac dinners and tormenting the King.
 
+var newDiv2 = document.createElement('div');
+newDiv2.id = 'hobbies';
+newDiv2.innerHTML = 'Long romantic walks on the beach, candle light Big Mac dinners and tormenting the King.';
+data.appendChild(newDiv2);
+
+
 //7. In the div id data section, create a div with an id of location and give it the following information: Honolulu, HI.
 
 //8. In the div id data section, create a div with an id of wants and give it the following information: Looking for a Mrs. McDonald.
